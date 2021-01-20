bb911ad1a3558b836c3bdff986cde9c3cc4d4094
Aaron Densmore <densmore98@gmail.com>
2021-01-19 16:28:38 -0800

    initial build commit

---
+++ Yard-Sale-BE/src/main/java/com/yardsalebe/YardSaleBeApplication.java
@@ -0,0 +1,13 @@
+package com.yardsalebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YardSaleBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YardSaleBeApplication.class, args);
+	}
+
+}
---
+++ Yard-Sale-BE/src/test/java/com/yardsalebe/YardSaleBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yardsalebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YardSaleBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
