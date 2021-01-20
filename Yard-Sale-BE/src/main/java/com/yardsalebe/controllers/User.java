89159fcc15f7048349691c1b58045ac32c3c2892
Aaron Densmore <densmore98@gmail.com>
2021-01-19 22:24:36 -0800

    security + User files have been mostly created

---
+++ Yard-Sale-BE/src/main/java/com/yardsalebe/controllers/User.java
@@ -0,0 +1,79 @@
+//This class creates a user that will be able to log into the software.
+//The user will be able to create and edit posts
+package com.yardsalebe.controllers;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="User")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String streetAddress;
+	private String city;
+	private String state;
+	private String zip;
+	@Column(nullable=false, unique = true)
+	private String username;
+	private String pass;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+}
---
+++ Yard-Sale-BE/src/main/java/com/yardsalebe/controllers/UserController.java
@@ -0,0 +1,5 @@
+package com.yardsalebe.controllers;
+
+public class UserController {
+
+}
